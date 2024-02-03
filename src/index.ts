ea6b5f153c54553c10dcb4906cee3d720449bfe4
AndrewGanzha <ganzha-2002@mail.com>
2024-02-03 20:31:34 +0300

    Add one new get request

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,16 @@ import express from "express";
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: any, res: any) => {
-  res.send("Hello Wor");
+const transports = [{id: 0, name: 'Фрунзенская'}, {id: 1, name: 'Костомароваская'}]
+
+app.get("/transports", (req: any, res: any) => {
+  res.send(transports);
+});
+
+app.get("/transports/:idCar", (req: any, res: any) => {
+  let request = transports.find(item => item.id === +req.params.idCar);
+
+  res.send(request);
 });
 
 app.listen(port, () => {
