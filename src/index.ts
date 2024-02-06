c4613bef1890e3e56d3462804760870078c052a6
AndrewGanzha <ganzha-2002@mail.com>
2024-02-06 12:37:29 +0300

    Add post request

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import express from "express";
+import bodyParser from "body-parser";
+import {randomUUID} from "crypto";
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 const transports = [{id: 0, name: 'Фрунзенская'}, {id: 1, name: 'Костомароваская'}]
+
+const parserMiddleware = bodyParser();
+
+app.use(parserMiddleware);
 
 app.get("/transports", (req: any, res: any) => {
   if(req.query.title) {
@@ -13,6 +20,16 @@ app.get("/transports", (req: any, res: any) => {
 
   res.send(transports);
 });
+
+app.post("/transports", (req: any, res: any) => {
+  const newTransport = { id: +(new Date()),  name: req.body.title.toString()};
+
+  transports.push(newTransport);
+
+
+  res.status(201).send(newTransport);
+});
+
 
 app.get("/transports/:id", (req: any, res: any) => {
   let transport = transports.find(item => item.id === +req.params.id);
