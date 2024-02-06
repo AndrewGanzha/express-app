f0ec66635ca32761f0311b6e105a22461a875e97
AndrewGanzha <ganzha-2002@mail.com>
2024-02-06 12:09:08 +0300

    Add query parameter

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,17 @@ const port = process.env.PORT || 3000;
 const transports = [{id: 0, name: 'Фрунзенская'}, {id: 1, name: 'Костомароваская'}]
 
 app.get("/transports", (req: any, res: any) => {
+  if(req.query.title) {
+    let searchString = req.query.title.toString();
+
+    res.send(transports.filter(transport => transport.name.indexOf(searchString) > -1));
+  }
+
   res.send(transports);
 });
 
-app.get("/transports/:idCar", (req: any, res: any) => {
-  let transport = transports.find(item => item.id === +req.params.idCar);
+app.get("/transports/:id", (req: any, res: any) => {
+  let transport = transports.find(item => item.id === +req.params.id);
 
   if(transport) {
     res.send(transport);
